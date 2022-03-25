c10151e0b82f6a23cebd3f85968976bd1215fcbd
curiousevil301073 <bagheldeepak554@gmail.com>
2022-03-25 16:46:26 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let currDate = new Date();
+currDate = currDate.getHours();
+let greeting = '';
+const cssStyle = { };
+
+if(currDate >= 1 && currDate < 12){
+  greeting = 'Good Morning';
+  cssStyle.color = 'green';
+}else if (currDate >=12 && currDate < 16){
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
+} else if( currDate >= 16 && currDate < 20){
+  greeting = 'Good Evening';
+  cssStyle.color = 'skyblue';
+}else{
+  greeting = 'Good Night';
+  cssStyle.color = 'black';
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <div>
+      <h1>Hello Sir, <span style = {cssStyle}> {greeting} </span></h1>
+    </div>
+  </>,
   document.getElementById('root')
 );
 
